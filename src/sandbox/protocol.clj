f56bf0114e37b3f199f7c35ac8c1bdfa599f8ee8
Eric Hogberg <misc@erichogberg.com>
2015-07-08 18:10:07 -0500

    IOFactory support for various reader/writer
    thingies...

--- src/sandbox/protocol.clj
+++ src/sandbox/protocol.clj
@@ -1,7 +1,8 @@
 (ns sandbox.protocol
-  (:import (java.io InputStream OutputStream
+  (:import (java.io InputStream OutputStream File
                     FileInputStream InputStreamReader BufferedReader
-                    FileOutputStream OutputStreamWriter BufferedWriter)))
+                    FileOutputStream OutputStreamWriter BufferedWriter)
+           (java.net Socket URL )))
 
 (defprotocol IOFactory
   "A protocol for things that can be read from and written to."
@@ -22,6 +23,27 @@
 (defn expectorate [dst content]
   (with-open [writer (make-writer dst)]
     (.write writer (str content))))
+
+
+(extend-protocol IOFactory
+  File
+  (make-reader [src]
+    (make-reader (FileInputStream. src)))
+  (make-writer [dst]
+    (make-writer (FileOutputStream. dst)))
+  Socket
+  (make-reader [src] (.getInputStream src))
+  (make-writer [dst] (.getOutputStream dst))
+  URL
+  (make-reader [src]
+    (if (= "file" (.getProtocol src))
+      (-> src .getPath FileInputStream.)
+      (.openStream src)))
+  (make-writer [dst]
+    (if (= "file" (.getProtocol dst))
+      (-> dst .getPath FileInputStream.)
+      (throw (IllegalArgumentException.
+              "Can't write to non-file URL")))))
 
 (extend InputStream
   IOFactory
