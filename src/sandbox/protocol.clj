9981642511f1245fec6fefe6f1f103774034aa04
Eric Hogberg <misc@erichogberg.com>
2015-07-08 21:55:19 -0500

    Behold, the mighty CryptoVault!

--- src/sandbox/protocol.clj
+++ src/sandbox/protocol.clj
@@ -1,8 +1,13 @@
 (ns sandbox.protocol
+  (:require [clojure.java.io :as io])
   (:import (java.io InputStream OutputStream File
                     FileInputStream InputStreamReader BufferedReader
                     FileOutputStream OutputStreamWriter BufferedWriter)
-           (java.net Socket URL )))
+           (java.net Socket URL )
+           (java.security KeyStore KeyStore$SecretKeyEntry
+                          KeyStore$PasswordProtection)
+           (javax.crypto KeyGenerator Cipher CipherOutputStream
+                         CipherInputStream)))
 
 (defprotocol IOFactory
   "A protocol for things that can be read from and written to."
@@ -61,17 +66,70 @@
                   (-> dst OutputStreamWriter. BufferedWriter.))})
 
 
-(deftype CryptoVault [filename keystore password])
-
-(def vault (->CryptoVault "vault-file" "keystore" "toomanysecrets"))
-
-(.filename vault)
-
-
 (defprotocol Vault
   (init-vault [vault])
   (vault-output-stream [vault])
   (vault-input-stream [vault]))
+
+(defn vault-key [vault]
+  (let [password (.toCharArray (.password vault))]
+    (with-open [fis (FileInputStream. (.keystore vault))]
+      (-> (doto (KeyStore/getInstance "JCEKS")
+            (.load fis password))
+          (.getKey "vault-key" password)))))
+
+(deftype CryptoVault [filename keystore password]
+  Vault
+  (init-vault [vault]
+    (let [password (.toCharArray (.password vault))
+          key (.generateKey (KeyGenerator/getInstance "AES"))
+          keystore (doto (KeyStore/getInstance "JCEKS")
+                     (.load nil password)
+                     (.setEntry "vault-key"
+                                (KeyStore$SecretKeyEntry. key)
+                                (KeyStore$PasswordProtection. password)))]
+      (with-open [fos (FileOutputStream. (.keystore vault))]
+        (.store keystore fos password))))
+
+  (vault-output-stream [vault]
+    (let [cipher (doto (Cipher/getInstance "AES")
+                   (.init Cipher/ENCRYPT_MODE (vault-key vault)))]
+      (CipherOutputStream. (io/output-stream (.filename vault)) cipher)))
+
+  (vault-input-stream [vault]
+    (let [cipher (doto (Cipher/getInstance "AES")
+                   (.init Cipher/DECRYPT_MODE (vault-key vault)))]
+      (CipherInputStream. (io/input-stream (.filename vault)) cipher)))
+
+  IOFactory
+  (make-reader [vault] (make-reader (vault-input-stream vault)))
+
+  (make-writer [vault] (make-writer (vault-output-stream vault))))
+
+
+(extend CryptoVault
+  clojure.java.io/IOFactory
+  (assoc clojure.java.io/default-streams-impl
+         :make-input-stream (fn [x opts] (vault-input-stream x))
+         :make-output-stream (fn [x opts] (vault-output-stream x))))
+
+(def vault (->CryptoVault "vault-file" "keystore" "toomanysecrets"))
+
+(init-vault vault)
+
+(expectorate vault "This is a test of the CryptoVault")
+
+(gulp vault)
+
+(spit vault "This is a test of the CryptoVault using spit and slurp")
+
+(slurp vault)
+
+
+
+
+
+
 
 
 
